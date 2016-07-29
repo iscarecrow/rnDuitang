094ff0f02a405b78100e89d9eedfae81f314221c
Hugin <hxjlucky@gmail.com>
2016-07-29 14:06:08 +0800

    add AlertIOS

--- app/containers/NavStore.js
+++ app/containers/NavStore.js
@@ -1,7 +1,10 @@
 'use strict';
 
 import React, { Component, PropTypes } from 'react';
-import { NavigatorIOS } from 'react-native';
+import { 
+  NavigatorIOS,
+  AlertIOS 
+} from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
@@ -10,6 +13,19 @@ import * as navigatorActions from '../actions/navigator';
 import Store from './Store';
 
 class NavStore extends Component {
+  _leftPress(){
+    AlertIOS.prompt(
+      '输入test',
+      null,
+      text => console.log("请输入test "+text)
+    );
+  }
+  _rightPress() {
+    AlertIOS.alert(
+     '购物车',
+     '暂时还没做webview前进统一的router'
+    );
+  }
   render() {
     const { navigatorData } = this.props;
     return (
@@ -18,7 +34,9 @@ class NavStore extends Component {
           component: Store,
           title: '堆糖商店',
           rightButtonIcon: require('../image/icon_cart/icon_cart.png'),
-          leftButtonIcon:require('../image/icon_membership/icon_membership_for_all.png')
+          leftButtonIcon:require('../image/icon_membership/icon_membership_for_all.png'),
+          onRightButtonPress: () => this._rightPress(),
+          onLeftButtonPress: ()=>this._leftPress(),
         }}
         style={{flex: 1}}
         tintColor='#333'
